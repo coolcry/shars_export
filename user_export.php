a48466dfa6b05f55f09172d31b04b15bd146ce58
coolcry <coolcry@gmail.com>
2014-12-12 15:42:59 +0800

    性能优化

--- user_export.php
+++ user_export.php
@@ -2,8 +2,6 @@
 include 'config.php';
 
 include 'mysql.class.php';
-
-
 class user {
 	private $source;
 	private $home_dic = HOME_DIC;
@@ -11,13 +9,38 @@ class user {
 		$this->source = new mysql ();
 		$this->source->connect ( DB_HOST, DB_USER, DB_PW, DB_NAME );
 	}
-	
-	public function member(){
+	public function prep_tables() {
+		{
+			$sql_d = "DROP table if EXISTS __email";
+			$sql_c = "CREATE TABLE `__email` ( `user_id` MEDIUMINT (9) NOT NULL, `value` VARCHAR (100) DEFAULT NULL, PRIMARY KEY (`user_id`)) ENGINE = INNODB DEFAULT CHARSET = utf8";
+			$sql_i = "INSERT INTO __email SELECT user_id, `value` FROM `user_vars` WHERE `name` = 'email'";
+			$this->source->query ( $sql_d );
+			$this->source->query ( $sql_c );
+			$this->source->query ( $sql_i );
+		}
 		
-		$dc = $this->us_ca();
+		{
+			$sql_d = "DROP table if EXISTS __first_name";
+			$sql_c = "CREATE TABLE `__first_name` ( `user_id` MEDIUMINT (9) NOT NULL, `value` VARCHAR (100) DEFAULT NULL, PRIMARY KEY (`user_id`)) ENGINE = INNODB DEFAULT CHARSET = utf8";
+			$sql_i = "INSERT INTO __first_name SELECT user_id, `value` FROM `user_vars` WHERE `name` = 'first_name'";
+			$this->source->query ( $sql_d );
+			$this->source->query ( $sql_c );
+			$this->source->query ( $sql_i );
+		}
+		{
+			$sql_d = "DROP table if EXISTS __last_name";
+			$sql_c = "CREATE TABLE `__last_name` ( `user_id` MEDIUMINT (9) NOT NULL, `value` VARCHAR (100) DEFAULT NULL, PRIMARY KEY (`user_id`)) ENGINE = INNODB DEFAULT CHARSET = utf8";
+			$sql_i = "INSERT INTO __last_name SELECT user_id, `value` FROM `user_vars` WHERE `name` = 'last_name'";
+			$this->source->query ( $sql_d );
+			$this->source->query ( $sql_c );
+			$this->source->query ( $sql_i );
+		}
+	}
+	public function member() {
+		$dc = $this->us_ca ();
 		
 		$sql = "SELECT
-	lower(email.email) as email,
+	lower(email.`value`) AS email,
 	'base' AS _website,
 	'default' AS _store,
 	'' AS confirmation,
@@ -25,10 +48,10 @@ class user {
 	'Default Store View' AS created_in,
 	'0' AS disable_auto_group_change,
 	'' AS dob,
-	firs_tname as firstname,
+	first_name.`value` AS firstname,
 	'' AS gender,
 	'1' AS group_id,
-last_name as lastname,
+	last_name.`value` AS lastname,
 	'' AS middlename,
 	'' AS password_hash,
 	'' AS prefix,
@@ -40,123 +63,101 @@ last_name as lastname,
 	'' AS suffix,
 	'' AS taxvat,
 	'' AS website_id,
-	'' AS password,
-ma.city as _address_city,
-'' as _address_company,
-ma.country as _address_country_id,
-ma.fax as _address_fax,
-first_name.firs_tname as _address_firstname,
-last_name.last_name as _address_lastname,
-'' as _address_middlename,
-ma.zip as _address_postcode,
-'' as _address_prefix,
-ma.state as _address_region,
-CONCAT(ma.address_1,\"\r\n\",ma.address_2)  as _address_street,
-'' as _address_suffix,
-ma.phone as _address_telephone,
-'' as _address_vat_id,
-ma.billing as _address_default_billing_,
-ma.shipping as _address_default_shipping_
+	'' AS `password`,
+	ma.city AS _address_city,
+	'' AS _address_company,
+	ma.country AS _address_country_id,
+	ma.fax AS _address_fax,
+	first_name.`value` AS _address_firstname,
+	last_name.`value` AS _address_lastname,
+	'' AS _address_middlename,
+	ma.zip AS _address_postcode,
+	'' AS _address_prefix,
+	ma.state AS _address_region,
+	CONCAT(
+		ma.address_1,
+		\"\r\n\",
+		ma.address_2
+	) AS _address_street,
+	'' AS _address_suffix,
+	ma.phone AS _address_telephone,
+	'' AS _address_vat_id,
+	ma.billing AS _address_default_billing_,
+	ma.shipping AS _address_default_shipping_
 FROM
 	users u
-LEFT JOIN (
-	SELECT
-		user_id,
-		`value` AS email
-	FROM
-		`user_vars`
-	WHERE
-		`name` = 'email'
-	GROUP BY
-		user_id
-) email ON email.user_id = u.id
-LEFT JOIN (
-	SELECT
-		user_id,
-		`value` AS firs_tname
-	FROM
-		`user_vars`
-	WHERE
-		`name` = 'first_name'
-	GROUP BY
-		user_id
-) first_name ON first_name.user_id = u.id
-LEFT JOIN (
-	SELECT
-		user_id,
-		`value` AS last_name
-	FROM
-		`user_vars`
-	WHERE
-		`name` = 'last_name'
-	GROUP BY
-		user_id
-) last_name ON last_name.user_id = u.id 
+LEFT JOIN __email email ON email.user_id = u.id
+LEFT JOIN __first_name first_name ON first_name.user_id = u.id
+LEFT JOIN __last_name last_name ON last_name.user_id = u.id
 LEFT JOIN member_addresses ma ON ma.user_id = u.id
-WHERE u.user_group_id = '2'
-order by email asc
-LIMIT ".LIMIT_VALUE ;
-		$arrs = $this->source->get_all_arr($sql);
+WHERE
+	u.user_group_id = '2'
+ORDER BY
+	email ASC
+LIMIT " . LIMIT_VALUE;
+		$arrs = $this->source->get_all_arr ( $sql );
 		
 		$fp = fopen ( $this->home_dic . 'member.csv', 'w' );
 		
-		fputcsv ( $fp, array_keys($arrs[0]) );
+		fputcsv ( $fp, array_keys ( $arrs [0] ) );
 		$last_email = '';
-		foreach ($arrs as $arr){
+		foreach ( $arrs as $arr ) {
 			
-			if($last_email != $arr['email']){	//如果是新的
-				$last_email = $arr['email'];
-			}else{
-				$arr['email'] = '';
-				$arr['_website'] = '';
-				$arr['_store'] = '';
-				$arr['confirmation'] = '';
-				$arr['created_at'] = '';
-				$arr['created_in'] = '';
-				$arr['disable_auto_group_change'] = '';
-				$arr['dob'] = '';
-				$arr['firstname'] = '';
-				$arr['gender'] = '';
-				$arr['group_id'] = '';
-				$arr['lastname'] = '';
-				$arr['middlename'] = '';
-				$arr['password_hash'] = '';
-				$arr['prefix'] = '';
-				$arr['reward_update_notification'] = '';
-				$arr['reward_warning_notification'] = '';
-				$arr['rp_token'] = '';
-				$arr['rp_token_created_at'] = '';
-				$arr['store_id'] = '';
-				$arr['suffix'] = '';
-				$arr['taxvat'] = '';
-				$arr['website_id'] = '';
-				$arr['password'] = '';
+			if ($last_email != $arr ['email']) { // 如果是新的
+				$last_email = $arr ['email'];
+			} else {
+				$arr ['email'] = '';
+				$arr ['_website'] = '';
+				$arr ['_store'] = '';
+				$arr ['confirmation'] = '';
+				$arr ['created_at'] = '';
+				$arr ['created_in'] = '';
+				$arr ['disable_auto_group_change'] = '';
+				$arr ['dob'] = '';
+				$arr ['firstname'] = '';
+				$arr ['gender'] = '';
+				$arr ['group_id'] = '';
+				$arr ['lastname'] = '';
+				$arr ['middlename'] = '';
+				$arr ['password_hash'] = '';
+				$arr ['prefix'] = '';
+				$arr ['reward_update_notification'] = '';
+				$arr ['reward_warning_notification'] = '';
+				$arr ['rp_token'] = '';
+				$arr ['rp_token_created_at'] = '';
+				$arr ['store_id'] = '';
+				$arr ['suffix'] = '';
+				$arr ['taxvat'] = '';
+				$arr ['website_id'] = '';
+				$arr ['password'] = '';
 			}
-			$arr['_address_telephone'] = $arr['_address_telephone'] ?$arr['_address_telephone']:'0';
-			$arr['_address_region'] = ($foo = $dc[$arr['_address_region']])?$foo:$arr['_address_region'];
-			fputcsv ( $fp, array_values( $arr) );
+			$arr ['_address_telephone'] = $arr ['_address_telephone'] ? $arr ['_address_telephone'] : '0';
+			$arr ['_address_region'] = ($foo = $dc [$arr ['_address_region']]) ? $foo : $arr ['_address_region'];
+			fputcsv ( $fp, array_values ( $arr ) );
 		}
 		
 		fclose ( $fp );
 	}
-	
-	private function us_ca(){
-		$dc = array();
-		$ca = file_get_contents('./ca.txt');
-		foreach (split("\r\n", $ca) as $r){
-			list($k,$v) = split("\t", $r);
-			$dc[$k] = $v;
-		} ;
+	private function us_ca() {
+		$dc = array ();
+		$ca = file_get_contents ( './ca.txt' );
+		foreach ( split ( "\r\n", $ca ) as $r ) {
+			list ( $k, $v ) = split ( "\t", $r );
+			$dc [$k] = $v;
+		}
+		;
 		
-		$us = file_get_contents('./us.txt');
-		foreach (split("\r\n", $us) as $r){
-			list($v,$k) = split("\t", $r);
-			$dc[$k] = $v;
-		} ;
+		$us = file_get_contents ( './us.txt' );
+		foreach ( split ( "\r\n", $us ) as $r ) {
+			list ( $v, $k ) = split ( "\t", $r );
+			$dc [$k] = $v;
+		}
+		;
 		
 		return $dc;
 	}
 }
 
-$user = new user();
+$user = new user ();
+$user->prep_tables ();
 $user->member();
